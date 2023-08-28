e20e8b7e82ad7bf1ea482a82a8708e7d4d8f835e
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-28 11:53:12 -0700

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,75 @@
+/**
+ * _swap - swap elements
+ * @val1: value 1
+ * @val2: value 2
+ */
+void _swap(int *val1, int *val2)
+{
+	int tmp;
+
+	tmp = *val1;
+	*val1 = *val2;
+	*val2 = tmp;
+}
+
+/**
+ * partition - partition
+ * @arr: array
+ * @start: start index
+ * @end: end index
+ * @size: array size
+ *
+ * Return: pivot
+ */
+int partition(int *arr, int start, int end, size_t size)
+{
+	int pivot;
+	int i;
+	int j;
+
+	pivot = arr[end];
+	i = (start - 1);
+	for (j = start; j <= end - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++;
+			_swap(&arr[i], &arr[j]);
+			print_array(arr, size);
+		}
+	}
+	_swap(&arr[i + 1], &arr[end]);
+	print_array(arr, size);
+	return (i + 1);
+}
+
+/**
+ * quick_sort_recursive - quick sort recursive
+ * @array: array
+ * @size: array size
+ * @start: starting index
+ * @end: ending index
+ */
+void quick_sort_recursive(int *array, size_t size, int start, int end)
+{
+	int pi;
+
+	if (start < end)
+	{
+		pi = partition(array, start, end, size);
+		quick_sort_recursive(array, size, start, pi - 1);
+		quick_sort_recursive(array, size, pi + 1, end);
+	}
+}
+
+/**
+ * quick_sort - quick sort
+ * @array: array
+ * @size: array size
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (size < 2)
+		return;
+	quick_sort_recursive(array, size, 0, size - 1);
+}
